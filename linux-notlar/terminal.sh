d8095fab33ce693502555e5acf66c0f134c9858d
metin-karakus <s.metinkarakus@gmail.com>
2023-02-17 16:50:01 +0300

    feat: docs update

--- linux-notlar/terminal.sh
+++ linux-notlar/terminal.sh
@@ -13,5 +13,4 @@ curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/instal
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
 echo "changing the plugins" && sed -i 's/\(^plugins=([^)]*\)/\1 zsh-autosuggestions zsh-syntax-highlighting/' .zshrc| bash && \
-cd ~ && source .zshrc && \
 echo "Done"
