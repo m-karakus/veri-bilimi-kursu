e3c342fc17e7cf95a49dafd6c98278827b0e151e
m-karakus <s.metinkarakus@gmail.com>
2023-07-25 22:51:06 +0200

    feat: trends

---
+++ python_calismalari/google_trends.py
@@ -0,0 +1,7 @@
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl="tr-TR", tz=-180, geo= "TR")
+df = pytrends.trending_searches(pn='turkey') #daily search term
+column_names = ['Topic']
+df.columns = column_names
+print(df)
